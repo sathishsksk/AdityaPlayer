e0a98d43521975c91556ec938db0c4dd93acec9b
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-07-23 18:09:44 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -8,9 +8,9 @@ que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "1677067"))
+API_HASH = getenv("API_HASH", "0d8c8aabe36b01db6a26a7f2780fa660")
+BOT_TOKEN = getenv("BOT_TOKEN", "5595298904:AAFQYLz6H4eia8yWykSjTSUVhFK6VWTfa68")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 STRING_SESSION = getenv("STRING_SESSION", "")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
